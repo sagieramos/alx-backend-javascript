adf01bc09fb18ff694755fb1eac3ea4557cebd12
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-04-21 02:04:47 +0100

    _

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,14 +1,13 @@
 /**
- * Represents a class room
+ * Represents a class room.
  */
 export default class ClassRoom {
 	/**
-	 * Creates a new `ClassRoom` instance.
-	 * 
-	 * @param {Number} maxStudentsSize - The max. number
-	 * of students that can be enrolled in the classroom.
+	 * Creates a new @see {@link ClassRoom}.
+	 * @param {Number} maxStudentsSize - The maximum number of
+	 * students in the class.
 	 */
 	constructor(maxStudentsSize) {
-		this._maxStudentsSize = maxStudentsSize;
+	  this._maxStudentsSize = maxStudentsSize;
 	}
-}
+  }
