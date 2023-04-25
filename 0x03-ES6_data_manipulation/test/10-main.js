e82331a45f8ea0a4ea5d5bd57b8324bf38e44067
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-04-25 19:15:24 +0100

    a function named updateUniqueItems that returns an updated map for all items with initial quantity at 1

--- 0x03-ES6_data_manipulation/test/10-main.js
+++ 0x03-ES6_data_manipulation/test/10-main.js
@@ -1,5 +1,5 @@
-import updateUniqueItems from "./10-update_uniq_items.js";
-import groceriesList from "./9-groceries_list.js";
+import updateUniqueItems from "../10-update_uniq_items.js";
+import groceriesList from "../9-groceries_list.js";
 
 const map = groceriesList();
 console.log(map);
